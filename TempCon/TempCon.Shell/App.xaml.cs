95ab165030f07ab4b5258497673bc9b669ec79e0
5Flags <avram@5fla.gs>
2015-06-12 06:44:11 +0100

    App outline set up

---
+++ TempCon/TempCon.Shell/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Media;
+using FirstFloor.ModernUI.Presentation;
+using GalaSoft.MvvmLight.Threading;
+using log4net;
+
+namespace TempCon.Shell
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static App()
+        {
+            DispatcherHelper.Initialize();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _log.Info("Application started.");
+
+            AppearanceManager.Current.ThemeSource = AppearanceManager.DarkThemeSource;
+            AppearanceManager.Current.AccentColor = Color.FromRgb(0x50, 0x2D, 0x7F);
+        }
+    }
+}
